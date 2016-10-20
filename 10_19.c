e95134591be3419d43e3eb3207ee47b7e216bd3f
oreo21 <reok99@gmail.com>
2016-10-20 15:23:45 -0400

    added files

---
+++ 10_19.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+  int a = 5;
+  printf("a = %d\n", a);
+  printf("a >> 1 = %d\n", a >> 1);
+  printf("a >> 2 = %d\n", a >> 2);
+  printf("a >> 3 = %d\n", a >> 3);
+  printf("a << 1 = %d\n", a << 1);
+  printf("a << 2 = %d\n", a << 2);
+  printf("a << 3 = %d\n", a << 3); 
+  return 0;
+}
