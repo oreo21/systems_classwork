91175ee83e07d889276977d84276e72c2bba1316
oreo21 <reok99@gmail.com>
2016-11-29 15:31:45 -0500

    added some lines

--- 11_29.c
+++ 11_29.c
@@ -1,19 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
+#include <sys/stat.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 #include <errno.h>
 
 int main(){
-	char text[100] = "i won't feed you";
+	int *a;
+	int status;
+	int sd;
+
+	sd = shmget(24601, sizeof(int), IPC_CREAT | 0644);
+	printf("sd: %d\n", sd);
+
 	int f = fork();
 	if (f == 0){
-		strcpy(text, "feed me!");
-		printf("child says: %s\n", text);
+		a = shmat(sd, 0, 0);
+		*a = 54;
+		printf("child says: %p\n", a);
+		printf("child says: %d\n", *a);
 	}
 	else {
-		wait(&f);
-		printf("parent says: %s\n", text);
+		wait(&status);
+		a = shmat(sd, 0, 0);
+		printf("parent says: %p\n", a);
+		printf("parent says: %d\n", *a);
 	}
 	return 0;
 }
